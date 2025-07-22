bc8b2f6ac5ae6220ad97d828153169e38ea17971
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-07-22 17:49:33 +0100

    feat: add a route to join the club, Attach user for all views as best practice

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -3,11 +3,15 @@ const usersRouter = Router() ;
 const usersController = require("../controllers/usersController");
 const passport = require("passport");
 
+// attach user for all views 
+usersRouter.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next()
+})
 
 usersRouter.get("/", (req, res)=> {
     res.render("main", { 
-        title: "Members Only",
-        user: req.user 
+        title: "Members Only"
     })
 })
 usersRouter.get("/sign-up", (req, res)=> res.render("sign-up"))
@@ -16,8 +20,12 @@ usersRouter.get("/login", (req, res)=> res.render("login", {
 }))
 
 usersRouter.get("/new-message", (req, res)=> res.render("newMessage", {
-    title: "New Message",
-    user: req.user
+    title: "New Message"
+}))
+
+// join the club
+usersRouter.get("/join-the-club", (req,res)=> res.render("join-the-club", {
+    title: "Join The Club"
 }))
 
 usersRouter.post("/sign-up", usersController.registerUser )
